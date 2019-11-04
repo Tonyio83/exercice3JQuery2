b5930d2a882f68bcac431a935cdcc4e0d7dfbdec
Tonyio83 <anthonyfrancelle@gmail.com>
2019-11-04 16:10:02 +0100

    exercice 3 terminé

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$('#show').click(function(){//crée un fonction au click du lien
+  $('#text').show();//affiche le texte
+});
+$('#hide').click(function(){//crée un fonction au click du lien
+  $('#text').hide();//cache le texte
+})
