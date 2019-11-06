964142c403fbad2b1c5f64c047906942f30ac579
Tonyio83 <anthonyfrancelle@gmail.com>
2019-11-06 09:12:01 +0100

    exercice 3 correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
-$('#show').click(function(){//crée un fonction au click du lien
-  $('#text').show();//affiche le texte
-});
-$('#hide').click(function(){//crée un fonction au click du lien
-  $('#text').hide();//cache le texte
+$(function(){
+  $('#show').click(function(){//appel de la fonction au clic du lien
+    $('#text').show();//affiche le texte
+  });
+  $('#hide').click(function(){//appel de la fonction au click du lien
+    $('#text').hide();//cache le texte
+  })
 })
